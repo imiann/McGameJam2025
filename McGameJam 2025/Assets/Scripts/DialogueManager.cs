64a8bfd6b15b50575a1b7c01f3fffe25a0225446
Ian McFarland <ian.mcfarland8@gmail.com>
2025-01-11 16:22:45 -0500

    Add first character assets

--- McGameJam 2025/Assets/Scripts/DialogueManager.cs
+++ McGameJam 2025/Assets/Scripts/DialogueManager.cs
@@ -3,6 +3,7 @@ using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
 using Unity.Cinemachine;
+using UnityEditor.PackageManager;
 
 public class DialogueManager : MonoBehaviour
 {
